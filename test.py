2ee65d96b102e7b64c123998c30fe3a289a23336
yixianlee2011 <yixian.lee.2011@smu.edu.sg>
2014-04-07 13:14:10 +0000

    Deploy 13

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ import main
 
 def test_functional():
     app = TestApp(main.bottle)
-    assert app.get('/api/get_bots').status == '200 OK'
+    assert app.get('/api/verify_service_m').status == '200 OK'
